aecd4683cd9d7d447dd26cb58fe7792374875c4e
Wisit Yoosabai <87866405+PhaiWisit@users.noreply.github.com>
2023-11-09 23:11:31 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-eerecord',
+      synchronize: true,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      autoLoadEntities: true
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,56 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) { }
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.findOne({
+      where: {
+        userName: createUserDto.userName,
+      },
+    });
+    if (user) {
+      throw new BadRequestException('Username is already use.');
+    }
+    return this.usersRepository.save(createUserDto);
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async getMe(username: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: {
+        userName: username,
+      },
+    });
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    return user;
+  }
+
+  findOne(id: number): Promise<User | null> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
